0f97090b6cb8c293efb2a11a558001bf9433c892
1kawdalg <1kawdalg@gmail.com>
2026-02-10 12:46:47 +0300

    Basic version of EmailService is ended

--- notification-service/src/main/java/dot/liberty/notification/service/EmailService.java
+++ notification-service/src/main/java/dot/liberty/notification/service/EmailService.java
@@ -2,6 +2,7 @@ package dot.liberty.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,21 @@ public class EmailService {
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom("noreply@dot.liberty");
+
+            mailSender.send(message);
+            log.info("Email sent to {}", to);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}: {}", to, e.getMessage());
+        }
+    }
 
 }
