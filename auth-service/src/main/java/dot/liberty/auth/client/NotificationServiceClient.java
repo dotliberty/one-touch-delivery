5c6eac4e73c1f3705d2e0d37e23e8e99e4c5a739
1kawdalg <1kawdalg@gmail.com>
2026-02-12 12:14:21 +0300

    Feign client of notification-service is realized

--- auth-service/src/main/java/dot/liberty/auth/client/NotificationServiceClient.java
+++ auth-service/src/main/java/dot/liberty/auth/client/NotificationServiceClient.java
@@ -1,4 +1,13 @@
 package dot.liberty.auth.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notification-service")
 public interface NotificationServiceClient {
+
+    @PostMapping("/api/notifications/send-email")
+    void sendEmail(@RequestBody SendEmailRequest request);
+
 }
---
+++ auth-service/src/main/java/dot/liberty/auth/client/SendEmailRequest.java
@@ -0,0 +1,18 @@
+package dot.liberty.auth.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendEmailRequest {
+
+    private String to;
+    private String subject;
+    private String body;
+
+}
