e4d0a2012d78cfbbebe9a31d80bb5f6f41beb6bf
1kawdalg <1kawdalg@gmail.com>
2026-02-13 17:08:18 +0300

    Verification endpoints are added to auth-controller

--- auth-service/src/main/java/dot/liberty/auth/controller/AuthController.java
+++ auth-service/src/main/java/dot/liberty/auth/controller/AuthController.java
@@ -1,10 +1,9 @@
 package dot.liberty.auth.controller;
 
-import dot.liberty.auth.dto.request.LoginRequest;
-import dot.liberty.auth.dto.request.RegisterRequest;
-import dot.liberty.auth.dto.request.ValidateTokenRequest;
+import dot.liberty.auth.dto.request.*;
 import dot.liberty.auth.dto.response.AuthResponse;
 import dot.liberty.auth.dto.response.ValidateTokenResponse;
+import dot.liberty.auth.dto.response.VerificationSentResponse;
 import dot.liberty.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,28 +22,40 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
-        AuthResponse response = authService.register(request);
+    public ResponseEntity<VerificationSentResponse> register(@Valid @RequestBody RegisterRequest request) {
+        VerificationSentResponse response = authService.register(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
     }
 
+    @PostMapping("/verify-email")
+    public ResponseEntity<AuthResponse> verifyEmail(@Valid @RequestBody VerifyEmailRequest request) {
+        AuthResponse response = authService.verifyEmail(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/resend-verification")
+    public ResponseEntity<VerificationSentResponse> resendVerification(@Valid @RequestBody ResendVerificationRequest request) {
+        VerificationSentResponse response = authService.resendVerificationCode(request);
+
+        return ResponseEntity.ok(response);
+    }
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
         AuthResponse response = authService.login(request);
 
-        return ResponseEntity
-                .ok(response);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/validate")
     public ResponseEntity<ValidateTokenResponse> validateToken(@Valid @RequestBody ValidateTokenRequest request) {
         ValidateTokenResponse response = authService.validateToken(request);
 
-        return ResponseEntity
-                .ok(response);
+        return ResponseEntity.ok(response);
     }
 
 }
